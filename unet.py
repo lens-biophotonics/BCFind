e75c61d7189a5fca9cdaa672611ad0ef3bfb6f45
Curzio Checcucci <curzio.checcucci@gmail.com>
2021-02-11 11:50:25 +0100

    Added get_config methods

--- unet.py
+++ unet.py
@@ -5,6 +5,9 @@ import tensorflow as tf
 class ConvBlock(tf.keras.layers.Layer):
     def __init__(self, n_filters, conv_size, conv_stride, **kwargs):
         super(ConvBlock, self).__init__(**kwargs)
+        self.n_filters = n_filters
+        self.conv_size = conv_size
+        self.conv_stride = conv_stride
 
         self.conv3D = tf.keras.layers.Conv3D(
             filters=n_filters,
@@ -17,16 +20,34 @@ class ConvBlock(tf.keras.layers.Layer):
 
     def call(self, inputs, training=None):
         h = self.conv3D(inputs)
-        if training:
-            h = self.batch_norm(h)
+        h = self.batch_norm(h, training=training)
         h = self.relu(h)
         return h
+
+    def get_config(self):
+        config = super(ConvBlock, self).get_config()
+        config.update(
+            {
+                "n_filters": self.n_filters,
+                "conv_size": self.conv_size,
+                "conv_stride": self.conv_stride,
+            }
+        )
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
 
 
 # Decoder block
 class InvConvBlock(tf.keras.layers.Layer):
     def __init__(self, n_filters, conv_size, conv_stride, activation, **kwargs):
         super(InvConvBlock, self).__init__(**kwargs)
+        self.n_filters = n_filters
+        self.conv_size = conv_size
+        self.conv_stride = conv_stride
+        self.activation = activation
 
         self.conv3D_T = tf.keras.layers.Conv3DTranspose(
             filters=n_filters,
@@ -40,17 +61,37 @@ class InvConvBlock(tf.keras.layers.Layer):
 
     def call(self, inputs, feat_concat=None, training=None):
         h = self.conv3D_T(inputs)
-        if training:
-            h = self.batch_norm(h)
+        h = self.batch_norm(h, training=training)
         h = self.activ(h)
         if feat_concat is not None:
             h = self.concat([h, feat_concat])
         return h
 
+    def get_config(self):
+        config = super(InvConvBlock, self).get_config()
+        config.update(
+            {
+                "n_filters": self.n_filters,
+                "conv_size": self.conv_size,
+                "conv_stride": self.conv_stride,
+                "activation": self.activation,
+            }
+        )
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
 
 class UNet(tf.keras.Model):
     def __init__(self, n_filters, e_size, e_stride, d_size, d_stride, **kwargs):
         super(UNet, self).__init__(**kwargs)
+        self.n_filters = n_filters
+        self.e_size = e_size
+        self.e_stride = e_stride
+        self.d_size = d_size
+        self.d_stride = d_stride
 
         # Encoder
         self.conv_block_1 = ConvBlock(n_filters, e_size, e_stride)
@@ -64,14 +105,31 @@ class UNet(tf.keras.Model):
         self.inv_conv_block_3 = InvConvBlock(n_filters, d_size, d_stride, "relu")
         self.inv_conv_block_4 = InvConvBlock(1, d_size, d_stride, "sigmoid")
 
-    def call(self, inputs, **kwargs):
-        h1 = self.conv_block_1(inputs, **kwargs)
-        h2 = self.conv_block_2(h1, **kwargs)
-        h3 = self.conv_block_3(h2, **kwargs)
-        h = self.conv_block_4(h3, **kwargs)
+    def call(self, inputs, training=None):
+        h1 = self.conv_block_1(inputs, training=training)
+        h2 = self.conv_block_2(h1, training=training)
+        h3 = self.conv_block_3(h2, training=training)
+        h = self.conv_block_4(h3, training=training)
 
-        h = self.inv_conv_block_1(h, feat_concat=h3, **kwargs)
-        h = self.inv_conv_block_2(h, feat_concat=h2, **kwargs)
-        h = self.inv_conv_block_3(h, feat_concat=h1, **kwargs)
-        h = self.inv_conv_block_4(h, **kwargs)
+        h = self.inv_conv_block_1(h, feat_concat=h3, training=training)
+        h = self.inv_conv_block_2(h, feat_concat=h2, training=training)
+        h = self.inv_conv_block_3(h, feat_concat=h1, training=training)
+        h = self.inv_conv_block_4(h, training=training)
         return h
+
+    def get_config(self):
+        config = super(UNet, self).get_config()
+        config.update(
+            {
+                "n_filters": self.n_filters,
+                "e_size": self.e_size,
+                "e_stride": self.e_stride,
+                "d_size": self.d_size,
+                "d_stride": self.d_stride,
+            }
+        )
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
