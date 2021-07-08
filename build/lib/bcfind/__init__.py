f7d0f9bf5221ca69abeb6f025bbc286f3b35e72a
Curzio Checcucci <curzio.checcucci@gmail.com>
2021-07-08 15:41:35 +0200

    Removed wheel dist

--- build/lib/bcfind/__init__.py
+++
@@ -1,5 +0,0 @@
-from bcfind.train import build_unet
-from bcfind.blob_dog import BlobDoG
-from bcfind.bcfind import BCFind
-from bcfind.data_generator import get_tf_data
-from bcfind.make_training_data import get_target
