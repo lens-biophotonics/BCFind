e05f34a3e3696f8583bf70f4386fbb20272e79f6
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2021-12-01 15:05:28 +0100

    Perform data augmentation on CPU

--- bcfind/data_augmentation.py
+++ bcfind/data_augmentation.py
@@ -1,170 +1,120 @@
-import cupy as cp
-import numpy as np
 import functools as ft
 
-from cucim.skimage import filters as cm_skim_filt
-from cucim.skimage import transform as cm_skim_trans
+import numpy as np
+from scipy import ndimage
 
-mempool = cp.get_default_memory_pool()
-pinned_mempool = cp.get_default_pinned_memory_pool()
+from skimage import filters
 
 
 def random_gamma(x_batch, gamma_range, p):
-    if cp.random.uniform(0, 1) < p:
-        x_batch = cp.asarray(x_batch)
-        gamma = cp.random.uniform(gamma_range[0], gamma_range[1])
-
-        new_x_batch = cp.zeros_like(x_batch)
-        for i, x in enumerate(x_batch):
-            x_min = x.min()
-            x_range = x.max() - x_min
-            new_x_batch[i] = cp.power((x - x_min) / x_range, gamma) * x_range + x_min
-        
-        new_x_batch = cp.asnumpy(new_x_batch)
-
-        del x_batch, gamma
-        mempool.free_all_blocks()
-        pinned_mempool.free_all_blocks()
-        return new_x_batch
-    else:
+    if np.random.uniform(0, 1) > p:
         return x_batch
+
+    gamma = np.random.uniform(gamma_range[0], gamma_range[1])
+
+    for i, x in enumerate(x_batch):
+        x_min = x.min()
+        x_range = x.max() - x_min
+        if x_range == 0:
+            continue
+        x_batch[i] = np.power((x - x_min) / x_range, gamma) * x_range + x_min
+
+    return x_batch
 
 
 def random_contrast(x_batch, alpha_range, p):
-    if cp.random.uniform(0, 1) < p:
-        alpha = cp.random.uniform(alpha_range[0], alpha_range[1])
-
-        new_x_batch = cp.zeros_like(x_batch)
-        for i, x in enumerate(x_batch):
-            x = cp.asarray(x)
-            x_mean = x.mean()
-            new_x_batch[i] = (x - x_mean) * alpha + x_mean
-
-        new_x_batch = cp.asnumpy(new_x_batch)
-
-        del x, alpha
-        mempool.free_all_blocks()
-        pinned_mempool.free_all_blocks()
-        return new_x_batch
-
-    else:
+    if np.random.uniform(0, 1) > p:
         return x_batch
+
+    alpha = np.random.uniform(alpha_range[0], alpha_range[1])
+
+    for i, x in enumerate(x_batch):
+        x_mean = x.mean()
+        x_batch[i] = (x - x_mean) * alpha + x_mean
+
+    return x_batch
 
 
 def random_brightness(x_batch, alpha_range, p):
-    if cp.random.uniform(0, 1) < p:
-        x_batch = cp.asarray(x_batch)
-        alpha = cp.random.uniform(alpha_range[0], alpha_range[1])
-
-        new_x_batch = x_batch + alpha
-        new_x_batch = cp.asnumpy(new_x_batch)
-
-        del x_batch, alpha
-        mempool.free_all_blocks()
-        pinned_mempool.free_all_blocks()
-        return new_x_batch
-
-    else:
+    if np.random.uniform(0, 1) > p:
         return x_batch
 
+    alpha = np.random.uniform(alpha_range[0], alpha_range[1])
 
-def random_zoom(x_batch, y_batch, alpha_range, p):
-    if cp.random.uniform(0, 1) < p:
-        alpha = np.random.uniform(alpha_range[0], alpha_range[1])
+    return x_batch + alpha
 
-        new_shape = np.array(x_batch.shape)
-        new_shape[1:-1] = np.round(new_shape[1:-1] * alpha).astype(int)
-        new_x_batch = cp.zeros(new_shape)
-        new_y_batch = cp.zeros(new_shape)
-        for i, (x, y) in enumerate(zip(x_batch, y_batch)):
-            x = cp.asarray(cp.squeeze(x))
-            y = cp.asarray(cp.squeeze(y))
 
-            new_x = cm_skim_trans.rescale(x, scale=alpha)
-            new_y = cm_skim_trans.rescale(y, scale=alpha)
-            new_x_batch[i] = new_x[..., cp.newaxis]
-            new_y_batch[i] = new_y[..., cp.newaxis]
-        
-        s0 = cp.random.randint(0, max(1, new_shape[1] - x_batch.shape[1]))
-        s1 = cp.random.randint(0, max(1, new_shape[2] - x_batch.shape[2]))
-        s2 = cp.random.randint(0, max(1, new_shape[3] - x_batch.shape[3]))
-        s = cp.array([s0, s1, s2])
-        e = s + cp.array(x_batch.shape[1:-1])
+def random_zoom(x_batch, alpha_range, p):
+    if alpha_range[0] < 1:
+        raise ValueError('Zoom factor must be > 1')
 
-        new_x_batch = new_x_batch[:, s[0]:e[0], s[1]:e[1], s[2]:e[2], :]
-        new_y_batch = new_y_batch[:, s[0]:e[0], s[1]:e[1], s[2]:e[2], :]
+    if np.random.uniform(0, 1) > p:
+        return x_batch
 
-        new_x_batch = cp.asnumpy(new_x_batch)
-        new_y_batch = cp.asnumpy(new_y_batch)
+    original_shape = np.array(x_batch.shape)
+    alpha = np.random.uniform(alpha_range[0], alpha_range[1])
 
-        del x, y, new_x, new_y, s0, s1, s2, s, e
-        mempool.free_all_blocks()
-        pinned_mempool.free_all_blocks()
-        return new_x_batch, new_y_batch
+    rank = len(x_batch.shape)
 
-    else:
-        return x_batch, y_batch
+    zoom = np.ones(rank)
+    zoom[1:-1] = alpha
+    x_batch = ndimage.zoom(x_batch, zoom, order=0, prefilter=False)
+
+    # random crop
+    high = x_batch.shape[1:-1] - original_shape[1:-1]
+    high[high == 0] = 1
+    f = np.random.randint(0, high)
+    t = f + original_shape[1:-1]
+
+    return x_batch[:, f[0]:t[0], f[1]:t[1], f[2]:t[2]]
 
 
 def random_gauss_filter(x_batch, sigma_range, p):
-    if cp.random.uniform(0, 1) < p:
-        sigma = np.random.uniform(sigma_range[0], sigma_range[1])
-
-        new_x_batch = cp.zeros_like(x_batch)
-        for i, x in enumerate(x_batch):
-            x = cp.asarray(x)
-            new_x_batch[i] = cm_skim_filt.gaussian(x, sigma=sigma)
-        
-        new_x_batch = cp.asnumpy(new_x_batch)
-
-        del x
-        mempool.free_all_blocks()
-        pinned_mempool.free_all_blocks()
-        return new_x_batch
-    else:
+    if np.random.uniform(0, 1) > p:
         return x_batch
+
+    sigma = np.random.uniform(sigma_range[0], sigma_range[1])
+
+    for i, x in enumerate(x_batch):
+        x_batch[i] = filters.gaussian(x, sigma=sigma)
+
+    return x_batch
 
 
 def random_noise(x_batch, sigma_range, p):
-    if cp.random.uniform(0, 1) < p:
-        x_batch = cp.asarray(x_batch)
-        sigma = cp.random.uniform(sigma_range[0], sigma_range[1])
-        noise = cp.random.normal(loc=0, scale=sigma, size=x_batch.shape)
-
-        new_x_batch = x_batch + noise
-
-        del x_batch, sigma, noise
-        mempool.free_all_blocks()
-        pinned_mempool.free_all_blocks()
-        return cp.asnumpy(new_x_batch)
-    else:
+    if np.random.uniform(0, 1) > p:
         return x_batch
 
+    sigma = np.random.uniform(sigma_range[0], sigma_range[1])
+    noise = np.random.normal(loc=0, scale=sigma, size=x_batch.shape)
 
-class Augmentor():
+    return x_batch + noise
+
+
+class Augmentor:
     def __init__(self, data_gen):
         self.data_gen = data_gen
         self.operations = dict()
         self.it = 0
-    
+
     def add_random_gamma(self, gamma_range, p=0.5):
         self.operations['gamma'] = ft.partial(random_gamma, gamma_range=gamma_range, p=p)
 
     def add_random_contrast(self, alpha_range, p=0.5):
         self.operations['contrast'] = ft.partial(random_contrast, alpha_range=alpha_range, p=p)
-    
+
     def add_random_brightness(self, alpha_range, p=0.5):
         self.operations['brightness'] = ft.partial(random_brightness, alpha_range=alpha_range, p=p)
 
     def add_random_zoom(self, alpha_range, p=0.5):
         self.operations['zoom'] = ft.partial(random_zoom, alpha_range=alpha_range, p=p)
-    
+
     def add_random_gauss_filter(self, sigma_range, p=0.5):
         self.operations['gauss_filter'] = ft.partial(random_gauss_filter, sigma_range=sigma_range, p=p)
-    
+
     def add_random_noise(self, sigma_range, p=0.5):
         self.operations['noise'] = ft.partial(random_noise, sigma_range=sigma_range, p=p)
-    
+
     def __len__(self):
         return len(self.data_gen)
 
@@ -172,18 +122,17 @@ class Augmentor():
         x, y = self.data_gen[idx]
         for op in self.operations:
             if op == 'zoom':
-                x, y = self.operations[op](x, y)
+                x = self.operations[op](x)
+                y = self.operations[op](y)
             else:
                 x = self.operations[op](x)
 
-        mempool.free_all_blocks()
-        pinned_mempool.free_all_blocks()
         return x, y
-    
+
     def __iter__(self):
         for item in (self[i] for i in range(len(self))):
             yield item
-    
+
     def __next__(self):
         if self.it <= len(self):
             item = self[self.it]
