a384a4bdc1d0b0face64b13a6c6f3b9375aa1a46
Kurtche <curzio.checcucci@gmail.com>
2022-04-14 15:25:43 +0200

    added args to TrainingDataset, fixed blobs in _img_generator method, more customizable augmentations

--- bcfind/augmentation.py
+++ bcfind/augmentation.py
@@ -8,23 +8,24 @@ import tensorflow as tf
 rng = np.random.default_rng()
 
 
-def random_crop(x, zoom_range=(0.8, 1.3), target_shape=(80, 240, 240)):
+def random_crop(x, y, zoom_range=None, target_shape=(50, 100, 100)):
     if zoom_range is not None:
         alpha = rng.uniform(zoom_range[0], zoom_range[1])
 
         rank = len(x.shape)
 
         zoom = np.ones(rank)
-        zoom[1:4] = alpha
+        zoom = alpha
         x = ndimage.zoom(x, zoom, order=0, prefilter=False)
 
-    high = np.array(x.shape[1:4]) - target_shape
+    high = np.array(x.shape) - target_shape
     high[high == 0] = 1
     f = rng.integers(0, high)
     t = f + target_shape
 
-    x = x[:, f[0]:t[0], f[1]:t[1], f[2]:t[2]]
-    return x
+    x = x[f[0]:t[0], f[1]:t[1], f[2]:t[2], np.newaxis]
+    y = y[f[0]:t[0], f[1]:t[1], f[2]:t[2], np.newaxis]
+    return x, y
 
 
 @tf.function
@@ -37,36 +38,38 @@ def random_gamma_tf(x, param_range=(0.5, 1.8)):
 
 
 @tf.function
-def random_noise_tf(x, param_range=(0.05, 0.15)):
+def random_noise_tf(x, param_range=(0.01, 0.05)):
     sigma = tf.random.uniform((1,), param_range[0], param_range[1])
     noise = tf.random.normal(x.shape, mean=0, stddev=sigma)
     return x + noise
 
 
 @tf.function
-def random_contrast_tf(x):
+def random_contrast_tf(x, param_range=(0, 2)):
     x_mean = tf.math.reduce_mean(x, axis=tuple(range(1, len(x.shape))), keepdims=True)
-    alpha = tf.random.uniform(x_mean.shape, 0, 2)
+    alpha = tf.random.uniform(x_mean.shape, param_range[0], param_range[1])
     return (x - x_mean) * alpha + x_mean
 
 
 @tf.function
-def random_brightness_tf(x, alpha_range=(-50, 100)):
-    return x + tf.random.uniform((x.shape[0],) + (1,) * (len(x.shape) - 1), alpha_range[0], alpha_range[1])
+def random_brightness_tf(x, param_range=(-50, 100)):
+    return x + tf.random.uniform((x.shape[0],) + (1,) * (len(x.shape) - 1), param_range[0], param_range[1])
 
 
 @tf.function
-def augment(x):
-    func_list = [
-        random_gamma_tf,
-        random_contrast_tf,
-        random_brightness_tf,
-        random_noise_tf,
-    ]
-
+def augment(x, func_list, p=0.5):
     branch = tf.random.shuffle(tf.range(len(func_list)))  # shuffle order of transformations
-    p = tf.random.uniform((len(func_list),), 0, 1)
-    cond = tf.math.less(p, [0.5] * len(func_list))  # conditions based on p < probability
+    random_p = tf.random.uniform((len(func_list),), 0, 1)
+    
+    if isinstance(p, float) and 0 <= p <= 1:
+        cond = tf.math.less(random_p, [p] * len(func_list))  # conditions based on p < probability
+    
+    elif isinstance(p, (list, tuple)) and len(p) == len(func_list):
+        cond = tf.math.less(random_p, p)  # conditions based on p < probability
+    
+    else:
+        raise ValueError('Augmentation probability must be a float between 0 and 1'\
+            'or a list of floats between 0 and 1 whose lenght is equal to augmentation operations.')
 
     for i in range(len(func_list)):
         # the following line must be inside for loop, not outside, to make the partial function use the new x
@@ -74,5 +77,4 @@ def augment(x):
         x = tf.cond(cond[i],
                     true_fn=lambda: tf.switch_case(branch[i], branch_fns),  # apply transformation
                     false_fn=lambda: x)  # do not apply transformation
-
     return x
--- bcfind/training_dataset.py
+++ bcfind/training_dataset.py
@@ -1,32 +1,113 @@
 import logging
-from pathlib import Path
-
+from multiprocessing.sharedctypes import Value
 import numpy as np
-
+import functools as ft
 import tensorflow as tf
+from pathlib import Path
 
 from zetastitcher import InputFile
 
 from bcfind.make_training_data import get_target
-from bcfind.augmentation import random_crop, augment
+from bcfind.augmentation import *
 
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-class TrainingDataset:
-    @staticmethod
-    def _img_generator(marker_list):
-        for marker_file in map(lambda x: Path(x.decode()), marker_list):
-            logger.info(f'loading {marker_file.with_suffix("")}')
-            input_image = InputFile(marker_file.with_suffix('')).whole().astype(np.float32)
-            logger.info('creating blobs')
-            blobs = get_target(marker_file, input_image.shape)
+def get_op_list(augmentations):
+    """ Returns a list of callable operations from a list or dictionary of default or custom augmentations.
 
+    Args:
+        augmentations (list, tuple, dict): list or tuple of strings/callables or both. String elements will use default values of implemented operations.
+                                    Strings must be one of [\'brightness\', \'contrast\', \'gamma\', \'noise\']. 
+                                    Callable elements will be called, better if they are tensorflow.functions.
+                                      dict of lists/callables or both. List values will be the parameter range of implemented operations. 
+                                    Keys of list values must be on of [\'brightness\', \'contrast\', \'gamma\', \'noise\'].
+                                    Callable values will be called, better if they are tensorflow.operations.
+                                    Keys of callable values must be different from implemented operation names.
+                                    Callables must take, in either cases, the tensor of an input image and return its augmented version.
+                                    
+    Raises:
+        ValueError: if args are bad specified.
+
+    Returns:
+        list: list of tensorflow callable operations.
+    """
+    implemented_ops = {
+            'gamma': random_gamma_tf,
+            'contrast': random_contrast_tf,
+            'brightness': random_brightness_tf,
+            'noise': random_noise_tf,
+            }
+    
+    ops_list = []
+    if isinstance(augmentations, (list, tuple)):
+        for op in augmentations:
+            if isinstance(op, str):
+                assert op in implemented_ops, f'{op} not allowed. Not in {implemented_ops}.'
+                ops_list.append(implemented_ops[op])
+
+            elif callable(op):
+                ops_list.append(op)
+
+            else:
+                raise ValueError(f'{op} is neither a string nor a callable.')
+    
+    elif isinstance(augmentations, dict):
+        for op in augmentations:
+            if op in implemented_ops:
+                assert len(augmentations[op]) == 2, f'{op} value must be of length 2.'
+                ops_list.append(ft.partial(implemented_ops[op], param_range=augmentations[op]))
+
+            elif callable(augmentations[op]):
+                ops_list.append(augmentations[op])
+            
+            else:
+                raise ValueError(f'{op} value is neither a list of parameter range for implemented operations nor a callable.')
+    else:
+        raise ValueError('augmentations is neither a list nor a dictionary.')
+
+    return ops_list
+
+
+class TrainingDataset:
+    """ Training dataset flow for 3D gray images with cell locations as targets.
+    Args:
+        tiff_list (list):
+        marker_list (list):
+        output_shape (list):
+        augmentations (optional, list, dict):
+        augmentations_prob (optional, float, list):
+
+    Returns:
+        tensorflow.datasets.Data: a tensorflow dataset.
+
+    Yields:
+        tensorflow.Tensor: batch of inputs and targets.
+    """
+    @staticmethod
+    def _img_generator(tiff_list, marker_list):
+        for marker_file in map(lambda x: Path(x.decode()), marker_list):
+            fname = marker_file.with_suffix('').name
+
+            input_file = [f for f in map(lambda x: Path(x.decode()), tiff_list) if f.name == fname]
+            
+            if len(input_file) > 1:
+                raise ValueError(f'{len(input_file)} tiff files have the same name.')
+            elif len(input_file) == 0:
+                raise ValueError(f'Tiff file {fname} not found!')
+
+            logger.info(f'loading {input_file[0]}')
+            input_image = InputFile(input_file[0]).whole().astype(np.float32)        
+
+            logger.info(f'creating blobs from {marker_file}')
+            blobs = get_target(marker_file, [input_image.shape[d] for d in [2, 1, 0]], default_radius=1.5)
+
+            blobs = blobs.transpose((2, 1, 0))
             yield input_image, blobs
 
-    def __new__(cls, marker_list):
+    def __new__(cls, tiff_list, marker_list, batch_size, output_shape, augmentations=None, augmentations_prob=0.5):        
         def _my_gen():
             for el in crop_gen:
                 yield el[0], el[1]
@@ -34,13 +115,47 @@ class TrainingDataset:
         img_gen = tf.data.Dataset.from_generator(
             cls._img_generator,
             output_signature=tf.TensorSpec(None, tf.float32),
-            args=(marker_list,))
+            args=(tiff_list, marker_list))
+
+        random_crop_wrap = ft.partial(random_crop, zoom_range=None, target_shape=output_shape)
 
         crop_gen = img_gen \
             .cache() \
             .shuffle(len(marker_list), reshuffle_each_iteration=True) \
-            .map(lambda x: tf.numpy_function(random_crop, [x], tf.float32))
+            .map(lambda el: tf.numpy_function(random_crop_wrap, [el[0], el[1]], (tf.float32, tf.float32)))
 
-        block_gen = tf.data.Dataset.from_generator(_my_gen, output_signature=(
-                (tf.TensorSpec(([80, 240, 240]), tf.float32),) * 2))
-        return block_gen.map(lambda x, y: (augment(x), y))
+        if augmentations is not None:
+            ops_list = get_op_list(augmentations)
+
+            block_gen = tf.data.Dataset.from_generator(_my_gen, output_signature=(
+                    (tf.TensorSpec((tuple(output_shape) + (1,)), tf.float32),) * 2))
+            block_gen = block_gen.map(lambda x, y: (augment(x, ops_list, augmentations_prob), y))
+            return block_gen.batch(batch_size)
+        
+        else:
+            return crop_gen.batch(batch_size)
+
+
+if __name__ == '__main__':
+    import os
+    
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+    
+    marker_dir = '/mnt/NASone/curzio/Data/I48/Gray_matter/GT_files/Train'
+    img_dir = '/mnt/NASone/curzio/Data/I48/Gray_matter/Tiff_files/Train'
+
+    marker_files = [f'{marker_dir}/{f}' for f in os.listdir(marker_dir)]
+    img_files = [f'{img_dir}/{f}' for f in os.listdir(img_dir)]
+
+    data = TrainingDataset(
+        img_files, 
+        marker_files, 
+        batch_size=2, 
+        output_shape=[50, 100, 100], 
+        augmentations=None,#('gamma', 'noise', lambda x: x - tf.reduce_min(x)),
+        augmentations_prob=1.0
+        )
+
+    for x, y in data:
+        print()
+        print(x.shape, tf.reduce_min(x, axis=[1, 2, 3, 4]).numpy())
