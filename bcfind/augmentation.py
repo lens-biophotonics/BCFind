861092cb7d281854bb0aa8f80a6d9c060a854fbd
Kurtche <curzio.checcucci@gmail.com>
2022-05-16 10:05:42 +0200

    added random flip, sitk zoom

--- bcfind/augmentation.py
+++ bcfind/augmentation.py
@@ -1,6 +1,6 @@
-import functools
+from argparse import ArgumentError
+import SimpleITK as sitk
 import numpy as np
-import functools as ft
 import tensorflow as tf
 
 from scipy import ndimage
@@ -8,206 +8,199 @@ from scipy import ndimage
 rng = np.random.default_rng()
 
 
+def random_crop(x, target_shape):
+    high = np.array(x.shape[-3:]) - target_shape
+    high[high == 0] = 1
+    f = rng.integers(0, high)
+    t = f + target_shape
+
+    output = x[..., f[0]:t[0], f[1]:t[1], f[2]:t[2]]
+    return output
+
+
 @tf.function(experimental_relax_shapes=True)
-def random_crop_tf(x, y, target_shape=(50, 100, 100)):
-    def random_crop(x, y, target_shape):
-        high = np.array(x.shape[-3:]) - target_shape
-        high[high == 0] = 1
-        f = rng.integers(0, high)
-        t = f + target_shape
-
-        x = x[..., f[0]:t[0], f[1]:t[1], f[2]:t[2]]
-        y = y[..., f[0]:t[0], f[1]:t[1], f[2]:t[2]]
-        return x, y
-
-    x, y = tf.numpy_function(random_crop, [x, y, target_shape], (tf.float32, tf.float32))
-    return tf.ensure_shape(x, x.shape), tf.ensure_shape(y, y.shape)
-
-
-@tf.function
-def random_zoom_tf(x, y, param_range=(1.0, 1.3)):
-    def scipy_zoom(x, y, param_range):
-        zoom = [rng.uniform(param_range[0], param_range[1])] * len(x.shape)
-        
-        if len(zoom) > 3:
-            zoom[0] = 1
-
-        x = ndimage.zoom(x, zoom, order=0, prefilter=False)
-        y = ndimage.zoom(y, zoom, order=0, prefilter=False)
-        x, y = random_crop_tf(x, y, x.shape[-3:])
-        return x, y
+def random_crop_tf(input, target_shape=(50, 100, 100)):    
+    output = tf.numpy_function(random_crop, [input, target_shape], tf.float32)
     
-    x_shape = x.shape
-    y_shape = y.shape
-    x, y = tf.numpy_function(scipy_zoom, [x, y, param_range], (tf.float32, tf.float32))
-    return tf.ensure_shape(x,  x_shape), tf.ensure_shape(y, y_shape)
+    input_shape = input.get_shape().as_list()
+    shape = (*input_shape[:len(input_shape) - 3], *target_shape)
+    return tf.ensure_shape(output, shape)
 
 
 @tf.function
-def random_rotation_tf(x, y, param_range=(-180, 180)):
-    def scipy_rotate(x, y):
-        angles = np.arange(param_range[0], param_range[1] + 1, 90)
+def random_zoom_tf(input, param_range=(1.0, 1.3), order=0, method='sitk'):
+    def sitk_zoom(x, param_range=param_range, order=order):
+        if order == 0:
+            interpolator = sitk.sitkNearestNeighbor
+        elif order == 1:
+            interpolator = sitk.sitkLinear
+        elif order == 2:
+            interpolator = sitk.sitkBSplineResamplerOrder2
+        elif order == 3:
+            interpolator = sitk.sitkBSplineResamplerOrder3
+        elif order == 4:
+            interpolator = sitk.sitkBSplineResamplerOrder4
+        elif order == 5:
+            interpolator = sitk.sitkBSplineResamplerOrder5
+        else:
+            raise ArgumentError('Order of interpolation must be between 0 and 5')
+
+        zoom_factor = rng.uniform(param_range[0], param_range[1])
+
+        def resample(volume):
+            img = sitk.GetImageFromArray(volume)
+            img_size = img.GetSize()
+
+            new_size = [int(round(img_size[i] * zoom_factor)) for i in range(len(img_size))]
+
+            resampler = sitk.ResampleImageFilter()
+            resampler.SetReferenceImage(img)
+            resampler.SetSize(new_size)
+            resampler.SetOutputSpacing(np.divide(img_size, new_size))
+            resampler.SetOutputOrigin(img.GetOrigin())
+            resampler.SetOutputDirection(img.GetDirection())
+            resampler.SetTransform(sitk.Transform(3, sitk.sitkIdentity))
+            resampler.SetInterpolator(interpolator)
+            resampler.SetDefaultPixelValue(0)
+            
+            new_volume = resampler.Execute(img)
+            new_volume = sitk.GetArrayFromImage(new_volume)
+            return new_volume
+        
+        if len(x.shape) > 3:
+            outputs = []
+            for i in range(x.shape[0]):
+                outputs.append(resample(x[i])[np.newaxis, ...])
+            output = np.concatenate(outputs, 0)
+
+        else:
+            output = resample(x)
+
+        output = random_crop(output, x.shape[-3:])
+        return output
+
+    output = tf.numpy_function(sitk_zoom, [input], tf.float32)
+    return tf.ensure_shape(output, input.shape)
+
+
+@tf.function
+def random_rotation_tf(input, param_range=(-180, 180), axes=(-2, -1)):
+    def scipy_rotate(x, param_range=param_range, axes=axes):
+        angles = np.arange(param_range[0], param_range[1], 90)
         angle = rng.choice(angles)
 
-        x = ndimage.rotate(x, angle, axes=(-2, -1), reshape=False)
-        y = ndimage.rotate(y, angle, axes=(-2, -1), reshape=False)
-        return x, y
+        if len(axes) > 2:
+            axes = rng.choice(axes, size=2, replace=False)
 
-    x_shape = x.shape
-    y_shape = y.shape
-    x, y =  tf.numpy_function(scipy_rotate, [x, y], (tf.float32, tf.float32))
-    return tf.ensure_shape(x,  x_shape), tf.ensure_shape(y, y_shape)
+        output = ndimage.rotate(x, angle, axes=axes, reshape=False)
+        return output
 
-
-@tf.function
-def random_flip_tf(x, y, param_range=(1, 2)):
-    def numpy_flip(x, y):
-        flip_dir = rng.choice(('lr', 'ud'))
-        axis = rng.choice(param_range)
-
-        if flip_dir == 'lr':
-            x = np.fliplr(x, axis=axis)
-            y = np.fliplr(y, axis=axis)
-        elif flip_dir == 'ud':
-            x = np.flipud(x, axis=axis)
-            y = np.flipud(y, axis=axis)
-        return x, y
-
-    x_shape = x.shape
-    y_shape = y.shape
-    x, y =  tf.numpy_function(numpy_flip, [x, y], (tf.float32, tf.float32))
-    return tf.ensure_shape(x,  x_shape), tf.ensure_shape(y, y_shape)
+    output =  tf.numpy_function(scipy_rotate, [input], tf.float32)
+    return tf.ensure_shape(output, input.shape)
 
 
 @tf.function
-def random_blur_tf(x, y, param_range=(0.01, 1.5)):
-    def scipy_blur(x):
+def random_flip_tf(input, axes=(1, 2, 3)):
+    def numpy_flip(x, axes=axes):
+        axis = rng.choice(axes)
+        output = np.flip(x, axis=axis)
+        return output
+
+    output =  tf.numpy_function(numpy_flip, [input], tf.float32)
+    return tf.ensure_shape(output, input.shape)
+
+
+@tf.function
+def random_blur_tf(input, param_range=(0.01, 1.0)):
+    def scipy_blur(x, param_range=param_range):
         sigma = [rng.uniform(param_range[0], param_range[1])] * len(x.shape)
+        
         if len(sigma) > 3:
-            sigma[0] = 0
+            sigma[:-3] = [0,] * (len(x.shape) - 3)
 
-        x = ndimage.gaussian_filter(x, sigma)
-        return x
+        output = ndimage.gaussian_filter(x, sigma)
+        return output
     
-    x_shape = x.shape
-    x = tf.numpy_function(scipy_blur, [x], tf.float32)
-    return tf.ensure_shape(x, x_shape), y
+    output = tf.numpy_function(scipy_blur, [input], tf.float32)
+    return tf.ensure_shape(output, input.shape)
 
 
 @tf.function
-def random_gamma_tf(x, y, param_range=(0.5, 1.8)):
+def random_gamma_tf(input, param_range=(0.8, 1.2)):
     gamma = tf.random.uniform((1,), param_range[0], param_range[1])
-    x_min = tf.math.reduce_min(x)
-    x_max = tf.math.reduce_max(x)
+    x_min = tf.math.reduce_min(input)
+    x_max = tf.math.reduce_max(input)
     x_range = x_max - x_min
-    return tf.math.pow((x - x_min) / x_range, gamma) * x_range + x_min, y
+
+    output = tf.math.pow((input - x_min) / x_range, gamma) * x_range + x_min
+    return output
 
 
 @tf.function
-def random_noise_tf(x, y, param_range=(0.01, 0.05)):
+def random_noise_tf(input, param_range=(0.001, 0.01)):
     sigma = tf.random.uniform((1,), param_range[0], param_range[1])
-    noise = tf.random.normal(tf.shape(x), mean=0, stddev=sigma)
-    return x + noise, y
+    noise = tf.random.normal(tf.shape(input), mean=0, stddev=sigma)
+    return input + noise
 
 
 @tf.function
-def random_contrast_tf(x, y, param_range=(0, 2)):
-    x_mean = tf.math.reduce_mean(x)
+def random_contrast_tf(input, param_range=(0, 2)):
+    x_mean = tf.math.reduce_mean(input)
     alpha = tf.random.uniform((1,), param_range[0], param_range[1])
-    return (x - x_mean) * alpha + x_mean, y
+    return (input - x_mean) * alpha + x_mean
 
 
 @tf.function
-def random_brightness_tf(x, y, param_range=(-50, 100)):
-    return x + tf.random.uniform((1,), param_range[0], param_range[1]), y
+def random_brightness_tf(input, param_range=(-0.1, 0.1)):
+    return input + tf.random.uniform((1,), param_range[0], param_range[1])
 
 
-def get_op_list(augmentations):
-    """ Returns a list of callable operations from a list or dictionary of default or custom augmentations.
-
-    Args:
-        augmentations (list, tuple, dict): list or tuple of strings/callables or both. String elements will use default values of implemented operations.
-                                    Strings must be one of [\'brightness\', \'contrast\', \'gamma\', \'noise\']. 
-                                    Callable elements will be called, better if they are tensorflow.functions.
-                                      dict of lists/callables or both. List values will be the parameter range of implemented operations. 
-                                    Keys of list values must be on of [\'brightness\', \'contrast\', \'gamma\', \'noise\'].
-                                    Callable values will be called, better if they are tensorflow.operations.
-                                    Keys of callable values must be different from implemented operation names.
-                                    Callables must take, in either cases, the tensor of an input image and return its augmented version.
-                                    
-    Raises:
-        ValueError: if args are bad specified.
-
-    Returns:
-        list: list of tensorflow callable operations.
-    """
-    implemented_ops = {
-            'gamma': random_gamma_tf,
-            'contrast': random_contrast_tf,
-            'brightness': random_brightness_tf,
-            'noise': random_noise_tf,
-            'rotation': random_rotation_tf,
-            'zoom': random_zoom_tf,
-            'blur': random_blur_tf,
-            }
+class Lambda(tf.keras.layers.Layer):
+    def __init__(self, func, args=None, **kwargs):
+        super(Lambda, self).__init__(**kwargs)
+        self._func = func
+        if args is not None:
+            self.args = args
+        else:
+            self.args = {}
     
-    ops_list = []
-    if isinstance(augmentations, (list, tuple)):
-        for op in augmentations:
-            if isinstance(op, str):
-                assert op in implemented_ops, f'{op} not allowed. Not in {implemented_ops}.'
-                ops_list.append(implemented_ops[op])
-
-            elif callable(op):
-                ops_list.append(op)
-
-            else:
-                raise ValueError(f'{op} is neither a string nor a callable.')
-    
-    elif isinstance(augmentations, dict):
-        for op in augmentations:
-            if op in implemented_ops:
-                assert len(augmentations[op]) == 2, f'{op} value must be of length 2.'
-                ops_list.append(ft.partial(implemented_ops[op], param_range=augmentations[op]))
-
-            elif callable(augmentations[op]):
-                ops_list.append(augmentations[op])
-            
-            else:
-                raise ValueError(f'{op} value is neither a list of parameter range nor a callable.')
-    else:
-        raise ValueError('augmentations is neither a list nor a dictionary.')
-
-    return ops_list
+    def call(self, input):
+        return self._func(input, **self.args)
 
 
 @tf.function
-def augment(x, y, func_list, p=0.5):
-    branch = tf.random.shuffle(tf.range(len(func_list)))  # shuffle order of transformations
-    random_p = tf.random.uniform((len(func_list),), 0, 1)
+def augment(xy, augmentations, p=0.3):
+    geometric_ops = ['rotation', 'flip', 'zoom', 'crop']
+
+    op_dict = get_op_dict(augmentations)
+    op_names = list(op_dict.keys())
+    op_index = np.arange(len(op_dict))
+    np.random.shuffle(op_index)  # shuffle order of transformations
     
+    random_p = np.random.uniform(0, 1, size=len(op_dict))
+    
+    # conditions based on p < probability
     if isinstance(p, float) and 0 <= p <= 1:
-        cond = tf.math.less(random_p, [p] * len(func_list))  # conditions based on p < probability
-    
-    elif isinstance(p, (list, tuple)) and len(p) == len(func_list):
-        cond = tf.math.less(random_p, p)  # conditions based on p < probability
-    
+        cond = random_p < [p,] * len(op_dict)
+    elif isinstance(p, (list, tuple)) and len(p) == len(op_dict):
+        cond = random_p < p 
     else:
         raise ValueError('Augmentation probability must be a float between 0 and 1 '\
-            'or a list of floats whose lenght is equal to augmentation operations.')
+            'or a list of floats whith lenght equal to augmentation operations.')
 
-    for i in range(len(func_list)):
-        # the following line must be inside for loop, not outside, to make the partial function use the new x
-        branch_fns = {j: functools.partial(func_list[j], x, y) for j in range(len(func_list))}
-        x, y = tf.cond(cond[branch[i]],
-                    true_fn=lambda: tf.switch_case(branch[i], branch_fns),  # apply transformation
-                    false_fn=lambda: (x, y),  # do not apply transformation
-        )
-    return x, y
+    # apply transformations in random order
+    for i in op_index:
+        if cond[i]:
+            if op_names[i] in geometric_ops:
+                xy = op_dict[op_names[i]](xy)
+            else:
+                x = op_dict[op_names[i]](xy[0])
+                xy = tf.concat((x[tf.newaxis], xy[1][tf.newaxis]), axis=0)
+    return xy
 
 
-def get_op_list(augmentations):
-    """ Returns a list of callable operations from a list or dictionary of default or custom augmentations.
+def get_op_dict(augmentations):
+    """ Returns a dict of callable operations from a list or dictionary of default or custom augmentations.
 
     Args:
         augmentations (list, tuple, dict): list or tuple of strings/callables or both. String elements will use default values of implemented operations.
@@ -226,24 +219,28 @@ def get_op_list(augmentations):
         list: list of tensorflow callable operations.
     """
     implemented_ops = {
-            'gamma': random_gamma_tf,
-            'contrast': random_contrast_tf,
-            'brightness': random_brightness_tf,
-            'noise': random_noise_tf,
-            'rotation': random_rotation_tf,
-            'zoom': random_zoom_tf,
-            'blur': random_blur_tf,
-            }
+        'crop': random_crop_tf,
+        'gamma': random_gamma_tf,
+        'contrast': random_contrast_tf,
+        'brightness': random_brightness_tf,
+        'noise': random_noise_tf,
+        'rotation': random_rotation_tf,
+        'flip': random_flip_tf,
+        'zoom': random_zoom_tf,
+        'blur': random_blur_tf,
+        }
     
-    ops_list = []
+    ops_dict = {}
     if isinstance(augmentations, (list, tuple)):
+        c = 0
         for op in augmentations:
             if isinstance(op, str):
-                assert op in implemented_ops, f'{op} not allowed. Not in {implemented_ops}.'
-                ops_list.append(implemented_ops[op])
+                assert op in implemented_ops, f'{op} not in {implemented_ops}.'
+                ops_dict[op] = Lambda(implemented_ops[op], augmentations[op], name=op)
 
             elif callable(op):
-                ops_list.append(op)
+                ops_dict[f'custom_op_{c}'] = op
+                c += 1
 
             else:
                 raise ValueError(f'{op} is neither a string nor a callable.')
@@ -251,15 +248,14 @@ def get_op_list(augmentations):
     elif isinstance(augmentations, dict):
         for op in augmentations:
             if op in implemented_ops:
-                assert len(augmentations[op]) == 2, f'{op} value must be of length 2.'
-                ops_list.append(ft.partial(implemented_ops[op], param_range=augmentations[op]))
+                ops_dict[op] = Lambda(implemented_ops[op], augmentations[op], name=op)
 
             elif callable(augmentations[op]):
-                ops_list.append(augmentations[op])
+                ops_dict[op] = augmentations[op]
             
             else:
                 raise ValueError(f'{op} value is neither a list of parameter range nor a callable.')
     else:
         raise ValueError('augmentations is neither a list nor a dictionary.')
 
-    return ops_list
+    return ops_dict
