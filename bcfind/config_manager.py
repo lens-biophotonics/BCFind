59dc8cf7c1b37b056e4d0a15497bc83a60c8a3a4
Kurtche <curzio.checcucci@gmail.com>
2022-05-25 11:31:27 +0200

    new input pipeline and unet model

--- bcfind/config_manager.py
+++ bcfind/config_manager.py
@@ -2,24 +2,10 @@ import yaml
 import numpy as np
 
 # Class to acces dictionary keys as if they were attributes
-# (some properties and methods of dictionary class are lost)
-class AttrDict:
-    def __init__(self, d):
-        self.d = d
-
-    def __getattr__(self, item):
-        return self.d[item]
-
-
-attr_to_yaml_key = {
-    'data': 'Dataset',
-    'exp': 'Experiment',
-    'preproc': 'PreProcessing',
-    'data_aug': 'DataAugmentation',
-    'unet': 'UNet',
-    'dog': 'DoG',
-    'vfv': 'VirtualFusedVolume',
-}
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
 
 class Configuration:
@@ -27,14 +13,24 @@ class Configuration:
         with open(yaml_file) as f:
             conf = yaml.load(f, Loader=yaml.FullLoader)
 
-        for k, v in attr_to_yaml_key.items():
+        yaml_key_to_attr = {
+            'Dataset': 'data',
+            'Experiment': 'exp',
+            'PreProcessing': 'preproc',
+            'DataAugmentation': 'data_aug',
+            'UNet': 'unet',
+            'DoG': 'dog',
+            'VirtualFusedVolume': 'vfv',
+        }
+        for k, v in yaml_key_to_attr.items():
             try:
-                setattr(self, k, AttrDict(conf[v]))
+                setattr(self, v, AttrDict(conf[k]))
             except KeyError:
                 pass
-
+        
+        # Dataset
         if self.data:
-            self.data.data_shape = np.array(self.data.data_shape)
+            self.data.shape = np.array(self.data.shape)
             self.data.dim_resolution = np.array(self.data.dim_resolution)
 
             self.data.train_tif_dir = f"{self.data.basepath}/Tiff_files/Train"
@@ -42,45 +38,48 @@ class Configuration:
             self.data.test_tif_dir = f"{self.data.basepath}/Tiff_files/Test"
             self.data.test_gt_dir = f"{self.data.basepath}/GT_files/Test"
 
-        if self.preproc.transpose is not None:
-            self.data.data_shape = np.array(
-                [self.data.data_shape[i] for i in self.preproc.transpose]
-            )
-            self.data.dim_resolution = np.array(
-                [self.data.dim_resolution[i] for i in self.preproc.transpose]
-            )
-            self.data.marker_columns = [
-                self.data.marker_columns[i] for i in self.preproc.transpose
-            ]
-
         if self.preproc.downscale is not None:
-            self.data.data_shape = np.ceil(
-                self.data.data_shape * self.preproc.downscale
+            self.data.shape = np.ceil(
+                self.data.shape * self.preproc.downscale
             ).astype(int)
 
             self.data.dim_resolution = self.data.dim_resolution / self.preproc.downscale
 
-        # Experiment configuration
-        self.exp.h5_dir = f"{self.exp.basepath}/{self.exp.name}/H5_files"
+        # Experiment
+        self.exp.basepath = f"{self.exp.basepath}/{self.exp.name}"
 
-        # PreProcessing configuration
+        # PreProcessing
         if self.preproc.normalization is None:
             self.preproc.normalization = "none"
 
         if self.preproc.standardization is None:
             self.preproc.standardization = "none"
 
-        # UNet configuration
+        # DataAugmentation
+        if self.data_aug.augment:
+            self.data_aug.op_args = {}
+            self.data_aug.op_probs = []
+            
+            for k, v in self.data_aug.items():
+                if k not in ['augment', 'op_args', 'op_probs']:
+                    self.data_aug.op_args[k] = v
+                    self.data_aug.op_probs.append(v['p'])
+                    del self.data_aug.op_args[k]['p']
+        else:
+            self.data_aug.op_args = None
+            self.data_aug.op_probs = None
+
+        # UNet
         self.unet.input_shape = self.unet.input_shape
 
         self.unet.checkpoint_dir = (
-            f"{self.exp.basepath}/{self.exp.name}/UNet_checkpoint"
+            f"{self.exp.basepath}/UNet_checkpoint"
         )
         self.unet.tensorboard_dir = (
-            f"{self.exp.basepath}/{self.exp.name}/UNet_tensorboard"
+            f"{self.exp.basepath}/UNet_tensorboard"
         )
 
-        # DoG configuration
+        # DoG
         self.dog.exclude_border = np.array(self.unet.exclude_border) + self.dog.exclude_border
 
         self.dog.logs_dir = f"{self.exp.basepath}/{self.exp.name}/DoG_logs"
@@ -89,16 +88,14 @@ class Configuration:
             f"{self.exp.basepath}/{self.exp.name}/{self.data.name}_predictions"
         )
 
-        # VirtualFusedVolume folders
+        # VirtualFusedVolume
         self.vfv.sub_shape = np.array(self.vfv.sub_shape)
         self.vfv.sub_overlap = np.array(self.vfv.sub_overlap)
 
         self.vfv.outdir = f"{self.vfv.outdir}/{self.vfv.name}"
         self.vfv.pred_outdir = f"{self.vfv.outdir}/stack_pred"
 
-    def __getattr__(self, item):
-        return None
-
 
 if __name__ == "__main__":
-    print(Configuration("../config.yaml").data.data_shape)
+    args = Configuration("/home/checcucci/Python/BCFind/config.yaml")
+    print(args.data_aug.op_args)
