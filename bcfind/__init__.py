4fa612da4aef2268661e7eef4cdc588d0f1e89e9
Kurtche <curzio.checcucci@gmail.com>
2022-06-08 16:55:55 +0200

    added shortcuts for models, dataflow and losses

--- bcfind/__init__.py
+++ bcfind/__init__.py
@@ -1,5 +1,7 @@
 from bcfind.models import UNet, SEUNet, ECAUNet
 from bcfind.blob_dog import BlobDoG
+from bcfind.training_dataset import TrainingDataset
+from bcfind.losses import FramedCrossentropy3D, FramedFocalCrossentropy3D
 
 from bcfind.bipartite_match import bipartite_match
 from bcfind.vfv_pred import predict_vfv
