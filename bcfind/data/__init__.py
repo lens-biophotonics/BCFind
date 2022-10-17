aff5d9de44b81715a3835dd953391ad7b8dc4d81
Kurtche <curzio.checcucci@gmail.com>
2022-10-17 17:31:39 +0200

    remove normalize_tf

--- bcfind/data/__init__.py
+++ bcfind/data/__init__.py
@@ -1,2 +1,2 @@
 from bcfind.data.artificial_targets import get_target
-from bcfind.data.training_dataset import TrainingDataset, get_input_tf, normalize_tf, get_target_tf
+from bcfind.data.training_dataset import TrainingDataset, get_input_tf, get_target_tf
