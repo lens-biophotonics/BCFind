439d5283a4193071a8af27967b9a41e40a64402f
Kurtche <curzio.checcucci@gmail.com>
2022-10-17 17:46:24 +0200

    add reduce argument

--- bcfind/losses/framed_crossentropy.py
+++ bcfind/losses/framed_crossentropy.py
@@ -4,19 +4,21 @@ from bcfind.losses.utils import get_mask_fn
 from bcfind.losses import DiceLoss
 
 
+@tf.keras.utils.register_keras_serializable(package='BCFind', name='FramedCrossentropy3D')
 class FramedCrossentropy3D(tf.keras.losses.Loss):
     """
     Implementation of binary crossentropy loss for 3D images where the predictions
     at the borders are not included in the computation.
     """
 
-    def __init__(self, border_size, target_shape, from_logits=False, add_dice=False, **kwargs):
+    def __init__(self, border_size, target_shape, from_logits=False, add_dice=False, reduce=None, **kwargs):
         super(FramedCrossentropy3D, self).__init__(**kwargs)
 
         self.border_size = border_size
         self.target_shape = target_shape
         self.from_logits = from_logits
         self.add_dice = add_dice
+        self.reduce = reduce
 
         self.bce = tf.keras.losses.BinaryCrossentropy(self.from_logits, reduction='none')
         self.mask_fn = get_mask_fn(self.target_shape, self.border_size)
@@ -24,26 +26,23 @@ class FramedCrossentropy3D(tf.keras.losses.Loss):
         if self.add_dice:
             self.dice = DiceLoss(from_logits=self.from_logits)
 
-    def __call__(self, y_true, y_pred, sample_weight=None):
-        ce = tf.keras.metrics.binary_crossentropy(
-            y_true, 
-            y_pred, 
-            from_logits=self.from_logits, 
-            label_smoothing=0.0, 
-            axis=-1
-        )
+    def call(self, y_true, y_pred):
+        loss = self.bce(y_true, y_pred)
         
         # framed loss
-        ce = tf.map_fn(self.mask_fn, ce)
-        ce = tf.reduce_mean(ce, axis=tf.range(1, tf.rank(ce)))
+        loss = tf.map_fn(self.mask_fn, loss)
+        loss = tf.reduce_mean(loss, axis=tf.range(1, tf.rank(loss)))
         
         # add dice loss
         if self.add_dice:
-            dice = self.dice(y_true, y_pred)
-            return ce + dice
+            dice = self.dice(tf.map_fn(self.mask_fn, y_true), tf.map_fn(self.mask_fn, y_pred))
+            loss = loss + dice
+        
+        # reduction
+        if self.reduce:
+            return tf.reduce_mean(loss, axis=0)
         else:
-            return ce
-            # return tf.reduce_mean(ce)
+            return loss
 
     def get_config(self):
         config = {
@@ -51,6 +50,7 @@ class FramedCrossentropy3D(tf.keras.losses.Loss):
             'target_shape': self.target_shape,
             'from_logits': self.from_logits,
             'add_dice': self.add_dice,
+            'reduce': self.reduce,
         }
         base_config = super(FramedCrossentropy3D, self).get_config()
         return dict(list(config.items()) + list(base_config.items()))
--- bcfind/losses/framed_focal_crossentropy.py
+++ bcfind/losses/framed_focal_crossentropy.py
@@ -3,12 +3,14 @@ import tensorflow as tf
 from bcfind.losses import DiceLoss
 from bcfind.losses.utils import get_mask_fn
 
+
+@tf.keras.utils.register_keras_serializable(package='BCFind', name='FramedFocalCrossentropy3D')
 class FramedFocalCrossentropy3D(tf.keras.losses.Loss):
     """
     Implementation of binary focal crossentropy loss for 3D images where the predictions
     at the borders are not included in the computation.
     """
-    def __init__(self, border_size, target_shape, from_logits=False, gamma=2.0, add_dice=False, **kwargs):
+    def __init__(self, border_size, target_shape, from_logits=False, gamma=2.0, add_dice=False, reduce=None, **kwargs):
         super(FramedFocalCrossentropy3D, self).__init__(**kwargs)
 
         self.border_size = border_size
@@ -16,6 +18,7 @@ class FramedFocalCrossentropy3D(tf.keras.losses.Loss):
         self.from_logits = from_logits
         self.gamma = gamma
         self.add_dice = add_dice
+        self.reduce = reduce
         
         self.focal_bce = tf.keras.losses.BinaryFocalCrossentropy(
             gamma=gamma,
@@ -29,18 +32,22 @@ class FramedFocalCrossentropy3D(tf.keras.losses.Loss):
         self.mask_fn = get_mask_fn(target_shape, border_size)
 
     def call(self, y_true, y_pred):
-        ce = self.focal_bce(y_true, y_pred)
+        loss = self.focal_bce(y_true, y_pred)
 
         # framed loss
-        ce = tf.map_fn(self.mask_fn, ce)
-        ce = tf.reduce_mean(ce, axis=tf.range(1, tf.rank(ce)))
+        loss = tf.map_fn(self.mask_fn, loss)
+        loss = tf.reduce_mean(loss, axis=tf.range(1, tf.rank(loss)))
         
         # add dice loss
         if self.add_dice:
-            dice = self.dice(y_true, y_pred)
-            return ce + dice
+            dice = self.dice(tf.map_fn(self.mask_fn, y_true), tf.map_fn(self.mask_fn, y_pred))
+            loss = loss + dice
+        
+        # reduction
+        if self.reduce:
+            return tf.reduce_mean(loss, axis=0)
         else:
-            return ce
+            return loss
     
     def get_config(self):
         config = {
@@ -49,6 +56,7 @@ class FramedFocalCrossentropy3D(tf.keras.losses.Loss):
             'from_logits': self.from_logits,
             'gamma': self.gamma,
             'add_dice': self.add_dice,
+            'reduce': self.reduce,
         }
         base_config = super(FramedFocalCrossentropy3D, self).get_config()
         return dict(list(config.items()) + list(base_config.items()))
