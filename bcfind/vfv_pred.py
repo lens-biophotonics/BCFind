b3522fe91e8bcea2cef6b0321764e898501c10de
kurtche <curzio.checcucci@gmail.com>
2024-02-23 18:28:37 +0100

    verbosity arg

--- bcfind/vfv_pred.py
+++ bcfind/vfv_pred.py
@@ -2,6 +2,7 @@ import os
 import re
 import json
 import shutil
+import logging
 import argparse
 import threading
 import numpy as np
@@ -29,7 +30,7 @@ def is_out_of_mask(box_coord, vfv_mask, vfv_shape):
     mask_shape = np.array(vfv_mask.shape)
     mask_rescale_factors = vfv_shape // mask_shape
     if (mask_rescale_factors != 1).all():
-        print(
+        logging.debug(
             f"""Found different shapes between VFV ({vfv_shape}) and mask ({mask_shape}). 
             Rescaling locations for mask indexing"""
         )
@@ -80,44 +81,44 @@ def put_substack_in_q(
 
     # Substack name to save and retrieve position in VFV
     sub_name = substack_name(z0, y0, x0, patch_shape, overlap)
-    print(f"\nRetrieving substack {sub_name}")
-    print(not_to_do)
+    logging.debug(f"\nRetrieving substack {sub_name}")
+
     if sub_name in not_to_do:
-        print("Already done! Skipping")
+        logging.debug("Already done! Skipping")
         return
 
     # mask handling
     if vfv_mask is not None:
-        print("Handling mask...")
+        logging.debug("Handling mask...")
         box_coord = ((z0, z1), (y0, y1), (x0, x1))
         mask_out = is_out_of_mask(box_coord, vfv_mask, vfv_shape)
 
         if mask_out:
-            print(f"Out of mask! Skipping")
+            logging.debug(f"Out of mask! Skipping")
             return
 
     substack = np.zeros(patch_shape)
     substack[: z1 - z0, : y1 - y0, : x1 - x0] = vfv[z0:z1, y0:y1, x0:x1]
 
     if np.mean(substack) < min_thresh:
-        print(f"Substack mean = {np.mean(substack)} < {min_thresh}. Skipping ")
+        logging.debug(f"Substack mean = {np.mean(substack)} < {min_thresh}. Skipping ")
         return
 
     if preprocessing_fun is not None:
-        print("Preprocessing...")
+        logging.debug("Preprocessing...")
         substack = preprocessing_fun(substack)
 
-    print("Putting in queue...")
+    logging.debug("Putting in queue...")
     queue.put([substack, sub_name])
     n = queue.qsize()
-    print(f"Substack_queue has {n} elements.")
+    logging.debug(f"Substack_queue has {n} elements.")
     return
 
 
 def find_cells(img, localizer, outfile=None):
     centers = localizer.predict(img)
     if outfile is not None:
-        print(f"Saving coordinates to {outfile}")
+        logging.debug(f"Saving coordinates to {outfile}")
         np.save(outfile, centers)
 
     return centers
@@ -131,7 +132,7 @@ def make_cloud(centers_dir, scale=None):
         try:
             centers = np.load(f"{centers_dir}/{f_name}")
         except ValueError as e:
-            print(f"DEBUG: error loading file {f_name}")
+            logging.info(f"DEBUG: error loading file {f_name}")
             raise Exception(e)
 
         if centers.shape[0] > 0:
@@ -182,7 +183,7 @@ def predict_vfv(
 
             substack, name = got
 
-            print(f"UNet prediction on substack {name}")
+            logging.debug(f"UNet prediction on substack {name}")
             nn_pred = nn_model(
                 substack[tf.newaxis, ..., tf.newaxis],
                 training=False,
@@ -191,7 +192,7 @@ def predict_vfv(
 
             embedding_q.put([nn_pred, name])
             n = embedding_q.qsize()
-            print(f"Embedding_queue has {n} elements")
+            logging.debug(f"Embedding_queue has {n} elements")
             substack_q.task_done()
 
     def localizer_worker():
@@ -204,11 +205,11 @@ def predict_vfv(
             nn_pred, name = got
             outfile = f"{outdir}/{name}.npy"
 
-            print(f"DoG prediction on substack {name}")
+            logging.debug(f"DoG prediction on substack {name}")
             cells = find_cells(nn_pred, localizer, outfile)
             embedding_q.task_done()
             if cells.shape[0] > 0:
-                print(f"{cells.shape[0]} cells found.")
+                logging.debug(f"{cells.shape[0]} cells found.")
 
     # Prepare threads for localizer predictions
     loc_threads = []
@@ -258,7 +259,7 @@ def predict_vfv(
                         )
 
     # Close threads
-    print("Closing threads")
+    logging.info("Closing threads")
     for q in range(sub_queue_size):
         substack_q.put(None)
     for q in range(emb_queue_size):
@@ -315,7 +316,7 @@ def mask_cloud_df(cloud_df, mask, vfv_shape):
 def save_point_cloud(
     pred_outdir, outfile, dim_resolution=(1, 1, 1), mask=None, vfv_shape=None
 ):
-    print("\nVFV predictions finished, making cloud...")
+    logging.info("\nVFV predictions finished, making cloud...")
     cloud_df = make_cloud(pred_outdir, dim_resolution)
 
     # If mask is given, remove predicted points outside of it
@@ -375,11 +376,26 @@ def parse_args():
         help="""Substacks whose mean is below this threshold will be discarded. 
         Default to 0.""",
     )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        default=False,
+        action="store_true",
+        help="""Set verbosity to True. If False (default) only load and save processes are printed. 
+        If True all substack processes will be printed.""",
+    )
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
+
+    if args.verbose:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+
+    logging.basicConfig(format="%(message)s", level=level)
 
     gpus = tf.config.list_physical_devices("GPU")
     tf.config.set_visible_devices(gpus[args.gpu], "GPU")
@@ -398,7 +414,7 @@ def main():
             shutil.copy(args.config, conf.vfv.outdir)
 
     # Preparing U-Net
-    print("\nLoading UNet and DoG parameters...")
+    logging.info("\nLoading UNet and DoG parameters...\n")
     unet = tf.keras.models.load_model(
         f"{conf.unet.checkpoint_dir}/model.tf", compile=False
     )
@@ -415,10 +431,10 @@ def main():
         dog_par = json.load(open(dog_par_file))
         dog.set_parameters(dog_par)
     else:
-        print("No optimal DoG parameteres found, assigning defaults.")
+        logging.info("No optimal DoG parameteres found, assigning defaults.\n")
 
     # Loading Virtual Fused Volume and optional mask
-    print("\nLoading VirtualFusedVolume...")
+    logging.info("\nLoading VirtualFusedVolume...\n")
     if conf.vfv.config_file.endswith(".yml"):
         zw.set_cache(LRUCache(maxsize=args.vfv_cache))
         vfv = VirtualFusedVolume(conf.vfv.config_file)
@@ -436,12 +452,12 @@ def main():
         conf.vfv.patch_shape,
         conf.vfv.patch_overlap,
     )
-    print(f"VirtualFusedVolume with shape = {vfv.shape}")
-    print(f"{n} patches to complete the whole prediction.")
+    logging.info(f"VirtualFusedVolume with shape = {vfv.shape}")
+    logging.info(f"{n} patches to complete the whole prediction.\n")
 
     if conf.vfv.mask_path is not None:
         vfv_mask = skio.imread(conf.vfv.mask_path)
-        print(
+        logging.info(
             f"""\nMask found! Shape = {vfv_mask.shape}, values = 
               {np.unique(vfv_mask)}"""
         )
@@ -449,7 +465,7 @@ def main():
         vfv_mask = None
 
     # Start predictions
-    print("\nStarting predictions...")
+    logging.info("\nStarting predictions...")
     s, e = int(args.start * n), int(args.end * n)
     predict_vfv(
         unet,
